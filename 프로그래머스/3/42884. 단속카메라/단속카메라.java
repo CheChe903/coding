7f0097a6a6068e78dce0bef44dbbb18759a40f72
KimJinYeongZ_ <137619133+KimJinYeongZ@users.noreply.github.com>
2024-03-20 20:41:06 +0900

    [level 3] Title: 단속카메라, Time: 19.43 ms, Memory: 54.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {    
+    public int solution(int[][] routes) {
+        int answer = 0;
+        int len = routes.length;
+        List<int[]> list = new ArrayList<>();
+        for(int[] route : routes) {
+            list.add(route);
+        }
+        Collections.sort(list, (a,b) -> a[1] - b[1]);
+        while(!list.isEmpty()) {
+            int position = list.get(0)[1];
+
+            for(int i = 0; i<list.size(); i++ ) {
+                if(list.get(i)[0]<=position){
+                    list.remove(i);
+                    i--;
+                } 
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
