d5b9ef0370cdd0325eb45a9a5d2e960ae86db29e
CheChe903 <137619133+CheChe903@users.noreply.github.com>
2024-08-29 17:33:42 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 324 ms, Memory: 67080 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		long [][] dp = new long[1001][1001];
+		long max = 0;
+
+		for(int i =0; i<n; i++) {
+			String input = br.readLine();
+
+			for(int j =0; j < m; j++) {
+				dp[i][j] = Long.parseLong(String.valueOf(input.charAt(j)));
+
+				if(dp[i][j] ==1 && j>0 && i>0) {
+					dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j],dp[i][j-1])) + dp[i][j];
+				}
+				if(max < dp[i][j])
+					max= dp[i][j];
+			}
+		}
+
+		System.out.println(max * max );
+
+	}
+
+}
