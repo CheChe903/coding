bc0959a7a75580763485f97556fcb7b77406a93c
CheChe903 <137619133+CheChe903@users.noreply.github.com>
2024-10-17 10:54:03 +0900

    [Silver V] Title: 창문 닫기, Time: 100 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int answer =0;
+
+
+        for(int i =1; i*i <=N; i++) {
+            answer++;
+        }
+        System.out.println(answer);
+    }
+}
