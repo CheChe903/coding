658e2e03c43a8bd9038457559ed4ab2cf8e554b1
CheChe903 <137619133+CheChe903@users.noreply.github.com>
2024-12-05 11:04:59 +0900

    [Silver I] Title: 떡 먹는 호랑이, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int D = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        if(D ==3) {
+            System.out.println(1);
+            System.out.println(K-1);
+
+        }
+        else {
+            int x =1, y=1;
+
+            for(int i =4; i<=D; i++) {
+                int temp =y;
+                y = x+y;
+                x= temp;
+            }
+            int size = K/y;
+
+            for(int i = size-1; i>=0; i--) {
+                if((K - (i*y)) % x ==0 ){
+                    System.out.println(((K - (i*y)) / x));
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+    }
+}
