56dd7a8164aabe0543dc3dddea8661a739b6d316
CheChe903 <137619133+CheChe903@users.noreply.github.com>
2025-11-10 23:42:13 +0900

    [Gold IV] Title: 연구소, Time: 844 ms, Memory: 297580 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,108 +1,92 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int arr[][];
-	static int dx[] = {-1,1 ,0,0};
-	static int dy[] = {0,0 ,-1,1};
-	static int N,M;
+    static int N,M;
+    static int dx[] = {-1,1,0,0};
+    static int dy[] = {0,0,-1,1};
+    static int answer;
+    public static void main(String[] args) throws Exception {
 
-	static int maxSafeZone = Integer.MIN_VALUE;
-	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		StringTokenizer st  = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		N = Integer.parseInt(st.nextToken());
+        answer =0;
 
-		M = Integer.parseInt(st.nextToken());
+        int arr[][] = new int[N][M];
 
-		arr = new int[N][M];
+        for(int i =0; i<N; i++) {
+            st =new StringTokenizer(br.readLine());
+            for(int j =0; j<M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        backtracking(0,arr);
 
-		for(int i =0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j<M; j ++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+        System.out.println(answer);
+    }
 
+    static void backtracking(int wallCount, int arr[][]) {
+        if(wallCount == 3) {
+            checkAnswer(arr);
+            return;
+        }
 
-		dfs(0);
-
-		System.out.println(maxSafeZone);
-
-
-	}
-
-	static void dfs(int wallCnt) {
-
-		if(wallCnt == 3) {
-			bfs();
-			return;
-		}
-
-
-		for(int i =0; i<N; i++) {
-			for(int j =0; j<M; j++ ){
-				if(arr[i][j] == 0) {
-					arr[i][j] = 1;
-					dfs(wallCnt+1);
-					arr[i][j] = 0;
-				} 
-			}
-		}
-	}
-
-	static void bfs() {
-		Queue<int []> queue = new LinkedList<>();
-
-
-		for(int i=0; i<N; i++) {
-			for(int j = 0; j<M; j++) {
-				if(arr[i][j] == 2) {
-					queue.add(new int[] { i,j});
-				}
-			}
-		}
-
-		int copyArr[][] = new int[N][M];
-
-		for(int i =0; i<N; i++) {
-			for(int j = 0; j<M; j++) {
-				copyArr[i][j] = arr[i][j];
-			}
-		}
-		while(!queue.isEmpty()) {
-			int [] now = queue.poll();
-			int x = now[0];
-			int y = now[1];
-
-			for(int k = 0; k<4; k++) {
-				int nx = x+ dx[k];
-				int ny  = y+ dy[k];
-
-				if(nx >=0 && nx<N && ny>=0 && ny <M) {
-					if(copyArr[nx][ny] == 0) {
-						queue.add(new int[]{ nx,ny});
-						copyArr[nx][ny] = 2;
-					}
-				}
-			}
-		}
-
-		curSafeZone(copyArr);
-	}
-
-	static void curSafeZone(int[][] copyArr) {
-		int safeZone =0;
-		for(int i =0; i<N; i++) {
-			for(int j = 0; j<M; j++) {
-				if(copyArr[i][j] ==0)
-					safeZone++;
-			}
-		}
-		if(maxSafeZone < safeZone) 
-			maxSafeZone= safeZone;
-	}
+        for(int i =0; i<N; i++) {
+            for(int j =0; j<M; j++) {
+                if(arr[i][j] ==0) {
+                    arr[i][j] = 1;
+                    backtracking(wallCount+1, arr);
+                    arr[i][j] = 0;
+                }
+            }
+        }
+    }
+    static void checkAnswer(int arr[][]) {
+    // 배열 복사
+    int temp[][] = new int[N][M];
+    for(int i = 0; i < N; i++) {
+        temp[i] = arr[i].clone();
+    }
+    
+    Queue<int[]> queue = new LinkedList<>();
+    
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) {
+            if(temp[i][j] == 2) {
+                queue.add(new int[] {i, j});
+            }
+        }
+    }
+    
+    while(!queue.isEmpty()) {
+        int now[] = queue.poll();
+        int nowx = now[0];
+        int nowy = now[1];
+        
+        for(int i = 0; i < 4; i++) {
+            int nx = nowx + dx[i];
+            int ny = nowy + dy[i];
+            
+            if(nx >= 0 && nx < N && ny >= 0 && ny < M && temp[nx][ny] == 0) {
+                temp[nx][ny] = 2;  // 복사본 수정
+                queue.add(new int[] {nx, ny});
+            }
+        }
+    }
+    
+    int count = 0;
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) {
+            if(temp[i][j] == 0) {
+                count++;
+            }
+        }
+    }
+    answer = Math.max(answer, count);
+}
 }
