600d4463ede1d7cd5cb59054ab03e0284380ed54
CheChe903 <137619133+CheChe903@users.noreply.github.com>
2025-11-10 19:51:10 +0900

    [level 2] Title: 문자열 압축, Time: 4.04 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,39 +1,32 @@
 class Solution {
     public int solution(String s) {
         int answer = s.length();
-        
-        int maxLength = s.length()/2;
-        
-        for(int i =1; i <=maxLength; i++) {
-        
-            String currentAnswer = "";    
-            int count  = 1;
-            String currentString = s.substring(0, i);
-            for(int j = i; j < s.length(); j+=i) {
-                String currentString2 = s.substring(j, Math.min(j + i, s.length()));
-                if(currentString.equals(currentString2)) {
-                    count+=1;
-                }
-                else {
-                    if(count !=1) {
-                        currentAnswer += count;
-                    }        
-                    currentAnswer += currentString;
-                    currentString = currentString2;
-                    count =1;
-                }
-            }
+        for(int i = 1; i<= s.length()/2; i++) { //i는 자를 갯수
+            // 맨 앞에부터 시작해서 크기만큼 잘라서 그걸로 이어가기 해야함.
             
-            if (count != 1) currentAnswer += count;
-            currentAnswer += currentString;
-            if(currentAnswer.length() < answer) {
-                answer = currentAnswer.length();
+            String sub = s.substring(0, i);
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            for(int j=i; j<s.length(); j+=i) {
+                int endIdx = Math.min(j + i, s.length());
+                String currentSub = s.substring(j, endIdx);
+                
+                if(sub.equals(currentSub)) {
+                    count++;
+                } else {
+                    if(count > 1) {
+                        sb.append(count);
+                    }
+                    sb.append(sub);
+                    count = 1;
+                    sub = currentSub;
+                }                               
             }
+            if(count >1) sb.append(count);
+            sb.append(sub);
+            answer = Math.min(sb.length(), answer);
         }
         
         return answer;
     }
 }
-
-// 1. 1개 자르는 것부터 n/2까지 진행
-// 2. 
